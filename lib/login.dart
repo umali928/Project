e76e3f213bcda0a594a9eb1c36e3183b9aac7b22
Prince Genel Umali <genelumali@gmail.com>
2025-03-16 23:22:29 +0800

    revised login

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -25,123 +26,122 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Login',
-                style: GoogleFonts.roboto(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF651D32),
-                ),
-              ),
-              const SizedBox(height: 6),
-              Text(
-                'Welcome to LSPU-Mart',
-                textAlign: TextAlign.center,
-                style: GoogleFonts.roboto(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 24),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              //PasswordField(),
-               const SizedBox(height: 12),
-                PasswordField(),
-              const SizedBox(height: 8),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Forgot your password?',
-                    style: GoogleFonts.roboto(
-                      color: Color(0xFF651D32),
-                      fontWeight: FontWeight.bold,
-                      ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 8),
-              Container(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      blurRadius: 10,
-                      offset: Offset(4, 6),
-                    ),
-                  ],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF651D32),
-                    foregroundColor: Colors.white,
-                    minimumSize: const Size(double.infinity, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: Text(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              double textFieldWidth = constraints.maxWidth > 600 ? 400 : double.infinity;
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
                     'Login',
+                    style: GoogleFonts.roboto(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF651D32),
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  Text(
+                    'Welcome to LSPU-Mart',
+                    textAlign: TextAlign.center,
                     style: GoogleFonts.roboto(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Create new account',
-                  style: GoogleFonts.roboto(color: Colors.grey),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Text('Or continue with',
-                style: GoogleFonts.roboto(
-                  color: Color(0xFF651D32),
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 12),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                   icon: Image.asset(
-                  'assets/googleicon.png',
-                   width: 40,
-                   height: 40,
+                  const SizedBox(height: 24),
+                  SizedBox(
+                    width: textFieldWidth,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
                   ),
+                  const SizedBox(height: 12),
+                  SizedBox(width: textFieldWidth, child: PasswordField()),
+                  const SizedBox(height: 8),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Forgot your password?',
+                        style: GoogleFonts.roboto(
+                          color: Color(0xFF651D32),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  SizedBox(
+                    width: textFieldWidth,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF651D32),
+                        foregroundColor: Colors.white,
+                        minimumSize: const Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        'Login',
+                        style: GoogleFonts.roboto(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Create new account',
+                      style: GoogleFonts.roboto(color: Colors.grey),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Or continue with',
+                    style: GoogleFonts.roboto(
+                      color: Color(0xFF651D32),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Image.asset(
+                          'assets/googleicon.png',
+                          width: 40,
+                          height: 40,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
-              ),
-            ],
+              );
+            },
           ),
         ),
       ),
     );
   }
 }
+
 class PasswordField extends StatefulWidget {
   @override
   _PasswordFieldState createState() => _PasswordFieldState();

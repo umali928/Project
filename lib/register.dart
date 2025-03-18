d4d38acba478478e9455eb85a99ca252900d32b2
Prince Genel Umali <genelumali@gmail.com>
2025-03-18 22:01:25 +0800

    revised register

--- lib/register.dart
+++ lib/register.dart
@@ -43,142 +43,181 @@ class _SignUpFormState extends State<SignUpForm> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start, // Align elements to the left
+      mainAxisAlignment: MainAxisAlignment.center, // Keep centered vertically
       children: [
-        Text(
-          'Create Account',
-          style: GoogleFonts.roboto(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF651D32),
+        Align(
+          alignment: Alignment.center, // Center title
+          child: Text(
+            'Create Account',
+            style: GoogleFonts.roboto(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF651D32),
+            ),
           ),
         ),
-        SizedBox(height: 5),
-        Text(
-          'Create an account so you can explore all of the products!',
-          textAlign: TextAlign.center,
-          style: GoogleFonts.roboto(
-            fontSize: 16,
-            color: Colors.black87,
-            fontWeight: FontWeight.bold,
+        SizedBox(height: 15), // Increased spacing
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0), // Adjust position
+          child: Text(
+            'Create an account so you can explore all of the products!',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.roboto(
+              fontSize: 18,
+              color: Colors.black87,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-        SizedBox(height: 20),
+        SizedBox(height: 65), // Increased spacing
         ConstrainedBox(
           constraints: BoxConstraints(maxWidth: 400),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // Align fields left
             children: [
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.black, width: 2),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                ),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                obscureText: !_isPasswordVisible,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.black, width: 2),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _isPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0), // Move email field right
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _isPasswordVisible = !_isPasswordVisible;
-                      });
-                    },
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                obscureText: !_isConfirmPasswordVisible,
-                decoration: InputDecoration(
-                  hintText: 'Confirm Password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.black, width: 2),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _isConfirmPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.black, width: 2),
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
-                      });
-                    },
+                    filled: true,
+                    fillColor: Colors.grey[200],
                   ),
                 ),
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF651D32),
-                  minimumSize: Size(double.infinity, 60),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+              SizedBox(height: 30), // More spacing
+              
+              // Password Field
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0), // Adjust password field position
+                child: TextField(
+                  obscureText: !_isPasswordVisible,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.black, width: 2),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                    ),
                   ),
                 ),
+              ),
+              SizedBox(height: 30), // More space before confirm password
+
+              // Confirm Password Field
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0), 
+                child: TextField(
+                  obscureText: !_isConfirmPasswordVisible,
+                  decoration: InputDecoration(
+                    hintText: 'Confirm Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.black, width: 2),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isConfirmPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30), // Move button down
+
+              // Sign Up Button
+              Align(
+                alignment: Alignment.center, // Center button
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF651D32),
+                    minimumSize: Size(250, 50), // Adjust button size
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign up',
+                    style: GoogleFonts.roboto(
+                      fontSize: 20, 
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              
+              SizedBox(height: 20), // Add space before "Already have an account"
+              Center(
                 child: Text(
-                  'Sign up',
-                  style: GoogleFonts.roboto(fontSize: 20, color: Colors.white,fontWeight: FontWeight.bold),
+                  'Already have an account',
+                  style: GoogleFonts.roboto(fontSize: 14, color: Color(0xFF651D32),fontWeight: FontWeight.bold),
                 ),
               ),
-              SizedBox(height: 10),
-              Text(
-                'Already have an account',
-                style: GoogleFonts.roboto(fontSize: 14, color: Colors.black54),
+              SizedBox(height: 30),
+              Center(
+                child: Text(
+                  'Or continue with',
+                  style: GoogleFonts.roboto(fontSize: 14, color: const Color.fromARGB(251, 0, 0, 0),fontWeight: FontWeight.bold),
+                ),
               ),
-              SizedBox(height: 20),
-              Text(
-                'Or continue with',
-                style: GoogleFonts.roboto(fontSize: 14, color: Colors.black54),
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: Image.asset(
-                      'assets/googleicon.png',
-                      width: 40,
-                      height: 40,
+              SizedBox(height: 15),
+
+              // Social Media Icons
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Image.asset(
+                        'assets/googleicon.png',
+                        width: 40,
+                        height: 40,
+                      ),
+                      onPressed: () {},
                     ),
-                    onPressed: () {},
-                  ),
-                ],
+                  ],
+                ),
               ),
             ],
           ),
@@ -187,3 +226,4 @@ class _SignUpFormState extends State<SignUpForm> {
     );
   }
 }
+

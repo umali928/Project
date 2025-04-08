1472310e4bd9480c3cf8e7c4831cd2a62ca58e6c
Prince Genel Umali <genelumali@gmail.com>
2025-04-08 23:12:19 +0800

    database working but not the image

--- lib/register.dart
+++ lib/register.dart
@@ -1,8 +1,17 @@
+// ... same imports
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'login.dart'; // Import LoginScreen
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+import 'login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(SignUpScreen());
 }
 
@@ -38,8 +47,110 @@ class SignUpForm extends StatefulWidget {
 }
 
 class _SignUpFormState extends State<SignUpForm> {
+  final TextEditingController fullNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   bool _isPasswordVisible = false;
   bool _isConfirmPasswordVisible = false;
+
+  Future<void> _register() async {
+    String fullName = fullNameController.text.trim();
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+    String confirmPassword = confirmPasswordController.text.trim();
+
+    if (fullName.isEmpty || email.isEmpty || password.isEmpty || confirmPassword.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please fill all fields')),
+      );
+      return;
+    }
+    if (!email.contains('@') || !email.contains('.')) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please enter a valid email')),
+     );
+       return;
+    }
+
+    if (password.length < 6) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Password must be at least 6 characters')),
+      );
+      return;
+    }
+    if (password != confirmPassword) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Passwords do not match')),
+      );
+      return;
+    }
+
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+        // Upload the default profile picture
+        // Upload the default profile picture to Firebase Storage
+      FirebaseStorage storage = FirebaseStorage.instance;
+      Reference ref = storage.ref().child('profile_pictures/${userCredential.user!.uid}.jpg');
+      
+      // Load the default profile picture from assets
+      Uint8List fileBytes = await rootBundle.load('assets/defaultprofile.jpg').then((byteData) => byteData.buffer.asUint8List());
+      
+      // Upload the profile picture
+      await ref.putData(fileBytes);
+      
+      // Get the download URL of the uploaded image
+      String profilePicUrl = await ref.getDownloadURL();
+      await _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'fullName': fullName,
+        'email': email,
+        'profilePicUrl': profilePicUrl, // Store the profile picture URL
+        'createdAt': Timestamp.now(),
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Account created successfully')),
+      );
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    } catch (e) {
+        String errorMessage = 'An error occurred. Please try again.';
+        if (e is FirebaseAuthException) {
+          switch (e.code) {
+            case 'email-already-in-use':
+              errorMessage = 'This email is already registered.';
+              break;
+            case 'invalid-email':
+              errorMessage = 'The email address is invalid.';
+              break;
+            case 'weak-password':
+              errorMessage = 'The password is too weak.';
+              break;
+            case 'unknown':
+              errorMessage = 'Something went wrong. Please check all fields.';
+              break;
+            default:
+              errorMessage = 'Error: ${e.message}';
+          }
+        } else {
+          errorMessage = 'Unexpected error: ${e.toString()}';
+        }
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(errorMessage)),
+        );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -75,12 +186,12 @@ class _SignUpFormState extends State<SignUpForm> {
         ConstrainedBox(
           constraints: BoxConstraints(maxWidth: 400),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // Align fields left
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Full Name Field
               Padding(
                 padding: const EdgeInsets.only(left: 10.0),
                 child: TextField(
+                  controller: fullNameController,
                   decoration: InputDecoration(
                     hintText: 'Full Name',
                     border: OutlineInputBorder(
@@ -89,8 +200,7 @@ class _SignUpFormState extends State<SignUpForm> {
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
-                      borderSide:
-                          BorderSide(color: Color(0xFF651D32), width: 2),
+                      borderSide: BorderSide(color: Color(0xFF651D32), width: 2),
                     ),
                     filled: true,
                     fillColor: Colors.grey[200],
@@ -99,9 +209,9 @@ class _SignUpFormState extends State<SignUpForm> {
               ),
               SizedBox(height: 30),
               Padding(
-                padding:
-                    const EdgeInsets.only(left: 10.0), // Move email field right
+                padding: const EdgeInsets.only(left: 10.0),
                 child: TextField(
+                  controller: emailController,
                   decoration: InputDecoration(
                     hintText: 'Email',
                     border: OutlineInputBorder(
@@ -110,20 +220,18 @@ class _SignUpFormState extends State<SignUpForm> {
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
-                      borderSide:
-                          BorderSide(color: Color(0xFF651D32), width: 2),
+                      borderSide: BorderSide(color: Color(0xFF651D32), width: 2),
                     ),
                     filled: true,
                     fillColor: Colors.grey[200],
                   ),
                 ),
               ),
-              SizedBox(height: 30), // More spacing
-
-              // Password Field
+              SizedBox(height: 30),
               Padding(
                 padding: const EdgeInsets.only(left: 10.0),
                 child: TextField(
+                  controller: passwordController,
                   obscureText: !_isPasswordVisible,
                   decoration: InputDecoration(
                     hintText: 'Password',
@@ -133,16 +241,13 @@ class _SignUpFormState extends State<SignUpForm> {
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
-                      borderSide:
-                          BorderSide(color: Color(0xFF651D32), width: 2),
+                      borderSide: BorderSide(color: Color(0xFF651D32), width: 2),
                     ),
                     filled: true,
                     fillColor: Colors.grey[200],
                     suffixIcon: IconButton(
                       icon: Icon(
-                        _isPasswordVisible
-                            ? Icons.visibility
-                            : Icons.visibility_off,
+                        _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
                       ),
                       onPressed: () {
                         setState(() {
@@ -154,10 +259,10 @@ class _SignUpFormState extends State<SignUpForm> {
                 ),
               ),
               SizedBox(height: 30),
-
               Padding(
                 padding: const EdgeInsets.only(left: 10.0),
                 child: TextField(
+                  controller: confirmPasswordController,
                   obscureText: !_isConfirmPasswordVisible,
                   decoration: InputDecoration(
                     hintText: 'Confirm Password',
@@ -167,21 +272,17 @@ class _SignUpFormState extends State<SignUpForm> {
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
-                      borderSide:
-                          BorderSide(color: Color(0xFF651D32), width: 2),
+                      borderSide: BorderSide(color: Color(0xFF651D32), width: 2),
                     ),
                     filled: true,
                     fillColor: Colors.grey[200],
                     suffixIcon: IconButton(
                       icon: Icon(
-                        _isConfirmPasswordVisible
-                            ? Icons.visibility
-                            : Icons.visibility_off,
+                        _isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
                       ),
                       onPressed: () {
                         setState(() {
-                          _isConfirmPasswordVisible =
-                              !_isConfirmPasswordVisible;
+                          _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
                         });
                       },
                     ),
@@ -189,12 +290,10 @@ class _SignUpFormState extends State<SignUpForm> {
                 ),
               ),
               SizedBox(height: 30),
-
-              // Sign Up Button
               Align(
-                alignment: Alignment.center, // Center button
+                alignment: Alignment.center,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: _register,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Color(0xFF651D32),
                     minimumSize: Size(double.infinity, 60),
@@ -212,7 +311,6 @@ class _SignUpFormState extends State<SignUpForm> {
                   ),
                 ),
               ),
-
               SizedBox(height: 20),
               Center(
                 child: GestureDetector(
@@ -237,21 +335,24 @@ class _SignUpFormState extends State<SignUpForm> {
                 child: Text(
                   'Or continue with',
                   style: GoogleFonts.poppins(
-                      fontSize: 14,
-                      color: const Color.fromARGB(251, 0, 0, 0),
-                      fontWeight: FontWeight.bold),
+                    fontSize: 14,
+                    color: const Color.fromARGB(251, 0, 0, 0),
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
               SizedBox(height: 15),
               OutlinedButton.icon(
                 onPressed: () {},
                 icon: Image.asset('assets/googleicon.png', height: 24),
-                label: Text('Continue with Google',
-                    style: GoogleFonts.poppins(
-                      color: Color(0xFF651D32),
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    )),
+                label: Text(
+                  'Continue with Google',
+                  style: GoogleFonts.poppins(
+                    color: Color(0xFF651D32),
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
                 style: OutlinedButton.styleFrom(
                   minimumSize: Size(double.infinity, 50),
                 ),
